328b169ae76398636a95fff9f8b2dc4fa7e83fef
kiharu3112 <kijiharu3112@outlook.jp>
2024-05-11 23:03:02 +0900

    feat:editor

---
+++ src/components/editor.tsx
@@ -0,0 +1,64 @@
+import { memo } from 'react';
+import AceEditor, { IMarker } from 'react-ace';
+import 'brace/mode/ruby';
+import 'brace/theme/xcode';
+
+const markers: IMarker[] = [
+  {
+    startRow: 3,
+    startCol: 1,
+    endRow: 4,
+    endCol: 1,
+    className: 'ruby-editor',
+    type: 'text',
+    inFront: true,
+  },
+];
+
+type Props = {
+  code: string;
+  setCode: (arg0: string) => void;
+};
+export const AceRubyEditor = memo<Props>((props) => {
+  const handleChange = (value: string) => {
+    props.setCode(value);
+  };
+
+  return (
+    <>
+      <style>
+        {`
+          .ace_gutter {
+            background: #0b6bcb2c !important; /* 新しいgutterの背景色 */
+          }
+      `}
+      </style>
+      <AceEditor
+        mode='ruby'
+        theme='xcode'
+        onChange={handleChange}
+        width='100%'
+        name='ace-editor'
+        editorProps={{ $blockScrolling: false }}
+        value={props.code}
+        showGutter={true}
+        highlightActiveLine={true}
+        showPrintMargin={true}
+        setOptions={{
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: true,
+          enableSnippets: true,
+          showLineNumbers: true,
+          tabSize: 2,
+        }}
+        style={{
+          width: '100%',
+          height: '70svh',
+          border: '1px solid #ddd',
+          borderRadius: '8px',
+        }}
+        markers={markers}
+      />
+    </>
+  );
+});
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,27 @@
-import {
-  Text
-} from '@chakra-ui/react';
-
+import { Box, Button, Flex, Image, Stack } from '@chakra-ui/react';
 import '../App.css';
+import { AceRubyEditor } from '../components/editor';
+import { useState } from 'react';
+import sendIcon from '/send-2.png';
 export const Home = () => {
+  const [code, setCode] = useState<string>(`
+def sample
+  puts "サンプルです"
+end
+`);
   return (
     <>
-      <Text>Hello</Text>
+      <Box w={'100%'} h={'100%'}>
+        <Stack m={'0.5rem'}>
+          <Flex align={"center"}>
+            <Button colorScheme='teal' size={'sm'}>
+              書き込む
+              <Image w={'1rem'} src={sendIcon} alt='送信' color={'white'} />
+            </Button>
+          </Flex>
+        </Stack>
+        <AceRubyEditor code={code} setCode={setCode} />
+      </Box>
     </>
   );
 };
