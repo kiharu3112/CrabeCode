5f3a2e4dea7bef0bb045f686a05fc49ed03f3bff
Miharup <74781355+kiharu3112@users.noreply.github.com>
2024-09-22 15:20:11 +0900

    chore:logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,8 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Image } from '@chakra-ui/react';
 export const Header = () => {
   return (
-    <Box as="b" w={'100%'} color="black">
+    <Box as="b" w={'100%'} mb={"1rem"} color="black" display="flex" alignItems="center" >
+      <Image src="/crabe.png" alt="crabe" h={"2rem"} objectFit={"cover"} />
       <Text fontSize={'3xl'}>Crabe Code</Text>
     </Box>
   );
